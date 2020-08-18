b184e1fbd34ccfb948a7a70852a8e37cb8e2da13
Selbi <43946617+heyselbi@users.noreply.github.com>
2020-08-18 11:12:18 -0400

    Create download_dataset.sh

---
+++ v0.5/classification_and_detection/download_dataset.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+mkdir dataset
+
+cd dataset
+
+wget -q https://zenodo.org/record/3236545/files/resnet34-ssd1200.pytorch
+
+pwd
+
+python -m pip install ck
+
+ck version
+
+ck pull repo:ck-env
+
+echo "install"
+
+ck install package --tags=image-classification,dataset,imagenet,val,original,full
+ck install package --tags=image-classification,dataset,imagenet,aux
+
+echo "locate"
+
+ck locate env --tags=image-classification,dataset,imagenet,val,original,full
+ck locate env --tags=image-classification,dataset,imagenet,aux
+
+echo "copy"
+
+cp `ck locate env --tags=aux`/val.txt `ck locate env --tags=val`/val_map.txt
+
+pwd
+ls -lt
+
+echo "coco"
+ck install package --tags=object-detection,dataset,coco,2017,val,original
+ck locate env --tags=object-detection,dataset,coco,2017,val,original
+
+pwd
+ls -lt
+
+cp -r /tekton/home/CK-TOOLS/* /root/v0.5/classification_and_detection/dataset/
+
+ls -lt /tekton/home/CK-TOOLS/
+ls -lt /tekton/home/CK-TOOLS/dataset-coco-2017-val
+ls -lt /root/v0.5/classification_and_detection/
+ls -lt /root/v0.5/classification_and_detection/dataset/
+
