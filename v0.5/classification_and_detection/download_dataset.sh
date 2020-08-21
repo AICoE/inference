cde4d765037ec2c2cc0c9e6595f5887dfac9cef3
Selbi <43946617+heyselbi@users.noreply.github.com>
2020-08-21 13:56:55 -0400

    Update download_dataset.sh

--- v0.5/classification_and_detection/download_dataset.sh
+++ v0.5/classification_and_detection/download_dataset.sh
@@ -6,8 +6,6 @@ cd dataset
 
 wget -q https://zenodo.org/record/3163026/files/ssd_mobilenet_v1_coco_2018_01_28.onnx
 
-pwd
-
 python -m pip install ck
 
 ck version
@@ -28,22 +26,10 @@ echo "copy"
 
 cp `ck locate env --tags=aux`/val.txt `ck locate env --tags=val`/val_map.txt
 
-pwd
-ls -lt
-
 echo "coco"
 ck install package --tags=object-detection,dataset,coco,2017,val,original
 ck locate env --tags=object-detection,dataset,coco,2017,val,original
 
-pwd
-ls -lt
-
 echo "copy all"
 cp -r /tekton/home/CK-TOOLS/* /root/v0.5/classification_and_detection/dataset/
-
-echo "directories"
-ls -lt /tekton/home/CK-TOOLS/
-ls -lt /tekton/home/CK-TOOLS/dataset-coco-2017-val
-ls -lt /root/v0.5/classification_and_detection/
-ls -lt /root/v0.5/classification_and_detection/dataset/
 
